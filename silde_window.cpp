3458f79b0579432e06ece2c27bc813b05f9eff85
ZhuJiaqi <zhujiaqi9905@gmail.com>
2020-10-23 16:28:08 +0800

    lab1 finish

--- silde_window.cpp
+++ silde_window.cpp
@@ -1,10 +1,6 @@
 #include "sysinclude.h"
 
-#include <stdlib.h>
-#include <string.h>
 #include<queue>
-#include<vector>
-#include<Winsock2.h>
 using std::queue;
 
 #define UINT8 int
@@ -35,223 +31,169 @@ typedef struct Frame_info {
 };
 
 
-int front = 0;
-int head = 0;
+//停等协议就是n比特重传的特殊情况
 int stu_slide_window_stop_and_wait(char* pBuffer, int bufferSize, UINT8 messageType) {
     static Frame_info frame_cache[WINDOW_SIZE_STOP_WAIT];
-    static int front = 0;//窗口起始位置
-    static int length = 0;//窗口中已发出的帧个数
-    static queue<Frame_info> waiting_queue;
+    static int front = 1;//窗口维护的起始seq_num
+    static int rear = 1;//窗口维护的结尾seq_num + 1
+    static queue<Frame_info> waiting_queue;//等待队列
     if(messageType == MSG_TYPE_SEND){
         //分配区域，缓存帧或者发出这个帧
         char *ptmp_frame=(char*)malloc(bufferSize);
-		memcpy(ptmp_frame, pBuffer);
+		memcpy(ptmp_frame, pBuffer, bufferSize);
         Frame_info fi;
         fi.pframe = (frame*)ptmp_frame;
         fi.size = bufferSize;
-        
-        if( length > WINDOW_SIZE_STOP_WAIT){//如果窗口大小过大
-            //帧放入等待队列
-            waiting_queue.push(fi);
-            return;
-
-        }else{//发送帧
-
-            frame_cache[(front + length) % WINDOW_SIZE_STOP_WAIT] = fi;
-            length++;
-            SendFRAMEPacket((unsigned char*)pBuffer, bufferSize);
-            return;
+        waiting_queue.push(fi);//帧放入等待队列
+        while( (rear - front) < WINDOW_SIZE_STOP_WAIT && !waiting_queue.empty()){//如果窗口小于最大窗口
+            Frame_info qf = waiting_queue.front();
+            waiting_queue.pop();
+            SendFRAMEPacket((unsigned char*)qf.pframe, qf.size);
+            frame_cache[rear % WINDOW_SIZE_STOP_WAIT] = qf;
+            rear++;
         }
     }
     else if(messageType == MSG_TYPE_TIMEOUT){
-        printf("MSG_TYPE_TIMEOUT\n");
         //得到要发的帧的序列号
         unsigned int seq_num = *((unsigned int*)pBuffer);
-        printf("the seq num is %d\n", seq_num);
-        int rear = front + length;
-        for(int i = seq_num; i < rear; ++i){
-            SendFRAMEPacket((unsigned char*) frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].size);
-
+        //重发超时帧之后的帧.在停等协议中只发送唯一那个帧
+        for(int i = front; i < rear; ++i){
+		    unsigned int k =  (unsigned int)ntohl(frame_cache[i % WINDOW_SIZE_STOP_WAIT].pframe->head.seq);
+            if(k == seq_num){
+                for(int j = i; j < rear; ++j){
+                     SendFRAMEPacket((unsigned char*) frame_cache[j % WINDOW_SIZE_STOP_WAIT].pframe, frame_cache[j % WINDOW_SIZE_STOP_WAIT].size);
+                }
+                break;
+            }
         }
-
-        return;
     }
-    else if(messageType == MSG_TYPE_RECEIVE){
+    else if(messageType == MSG_TYPE_RECEIVE){     
         frame* received_frame = (frame*)pBuffer;
-        unsigned int ack_num = (unsigned int)ntohl(received_frame->head.ack);
-        printf("receive frame, the ack num is %d\n", ack_num);
+        unsigned int seq_num = (unsigned int)ntohl(received_frame->head.ack);
         frame_kind fkind = (frame_kind)(ntohl((unsigned long)received_frame->head.kind));
-        int rear = front + length;
         if(fkind == ack){//返回ack
-            if(ack_num >= front && ack_num < rear){
-                length -= ack_num - front + 1;//窗口缩小
-                front = ack_num + 1;
+            if(seq_num >= front && seq_num < rear){
+                front = seq_num + 1;
                 
                 //指导书写的发一个帧，其实应该尽量都发完
-                while(length <= WINDOW_SIZE_STOP_WAIT && !waiting_queue.empty()){
+                while((rear - front) < WINDOW_SIZE_STOP_WAIT && !waiting_queue.empty()){
                     Frame_info fi = waiting_queue.front();
                     waiting_queue.pop();
-                    frame_cache[(front + length) % WINDOW_SIZE_STOP_WAIT] = fi;
+                    frame_cache[rear % WINDOW_SIZE_STOP_WAIT] = fi;
                     SendFRAMEPacket((unsigned char* )fi.pframe, fi.size);
-                    length++;
+                    rear++;
                 }
             }
-
         }else if(fkind == nak){//nak,重发
-            printf("in received, fkind is nak\n");
-            for(int i = ack_num; i < rear; ++i){
+            for(int i = seq_num; i < rear; ++i){
                 SendFRAMEPacket((unsigned char *)frame_cache[i % WINDOW_SIZE_STOP_WAIT].pframe, frame_cache[i % WINDOW_SIZE_STOP_WAIT].size);
             }
         }
-
     }
+	return 0;
+
+
 }
 
 int stud_slide_window_back_n_frame(char* pBuffer, int bufferSize, UINT8 messageType){
     static Frame_info frame_cache[WINDOW_SIZE_BACK_N_FRAME];
-    static int front = 0;//窗口起始位置
-    static int length = 0;//窗口中已发出的帧个数
+    static int front = 1;//窗口维护的起始seq_num
+    static int rear = 1;//窗口维护的结尾seq_num+1
     static queue<Frame_info> waiting_queue;
     if(messageType == MSG_TYPE_SEND){
         //分配区域，缓存帧或者发出这个帧
         char *ptmp_frame=(char*)malloc(bufferSize);
-		memcpy(ptmp_frame, pBuffer);
+		memcpy(ptmp_frame, pBuffer, bufferSize);
         Frame_info fi;
         fi.pframe = (frame*)ptmp_frame;
         fi.size = bufferSize;
-        
-        if( length > WINDOW_SIZE_BACK_N_FRAME){//如果窗口大小过大
-            //帧放入等待队列
-            waiting_queue.push(fi);
-            return;
-
-        }else{//发送帧
-
-            frame_cache[(front + length) % WINDOW_SIZE_BACK_N_FRAME] = fi;
-            length++;
-            SendFRAMEPacket((unsigned char*)pBuffer, bufferSize);
-            return;
+        waiting_queue.push(fi);//帧放入等待队列
+        while( (rear - front) < WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){//如果窗口小于最大窗口
+            Frame_info qf = waiting_queue.front();//从等待队列中取出帧
+            waiting_queue.pop();
+            SendFRAMEPacket((unsigned char*)qf.pframe, qf.size);//发送帧
+            frame_cache[rear % WINDOW_SIZE_BACK_N_FRAME] = qf;
+            rear++;
         }
     }
     else if(messageType == MSG_TYPE_TIMEOUT){
-        //得到要发的帧的序列号
-        unsigned int seq_num = *((unsigned int*)pBuffer);
-        printf("the seq num is %d\n", seq_num);
-        int rear = front + length;
-        for(int i = seq_num; i < rear; ++i){
+        //指导书写着应该从超时的帧开始发。但实际上是发送所有的未确认帧
+         for(int i = front; i < rear; ++i){
             SendFRAMEPacket((unsigned char*) frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].size);
-
         }
-
-        return;
     }
     else if(messageType == MSG_TYPE_RECEIVE){
         frame* received_frame = (frame*)pBuffer;
-        unsigned int ack_num = (unsigned int)ntohl(received_frame->head.ack);
-        printf("receive frame, the ack num is %d\n", ack_num);
+        unsigned int seq_num = (unsigned int)ntohl(received_frame->head.ack);
         frame_kind fkind = (frame_kind)(ntohl((unsigned long)received_frame->head.kind));
-        int rear = front + length;
         if(fkind == ack){//返回ack
-            if(ack_num >= front && ack_num < rear){
-                length -= ack_num - front + 1;//窗口缩小
-                front = ack_num + 1;
-                
+            if(seq_num >= front && seq_num < rear){
+                front = seq_num + 1;
                 //指导书写的发一个帧，其实应该尽量都发完
-                while(length <= WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){
+                while((rear - front) < WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){
                     Frame_info fi = waiting_queue.front();
                     waiting_queue.pop();
-                    frame_cache[(front + length) % WINDOW_SIZE_BACK_N_FRAME] = fi;
+                    frame_cache[rear % WINDOW_SIZE_BACK_N_FRAME] = fi;
                     SendFRAMEPacket((unsigned char* )fi.pframe, fi.size);
-                    length++;
+                    rear++;
                 }
             }
 
-        }else if(fkind == nak){//nak,重发
-            printf("in received, fkind is nak\n");
-            for(int i = ack_num; i < rear; ++i){
-                SendFRAMEPacket((unsigned char *)frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].size);
-            }
         }
-
     }
+	return 0;
+
 }
 
 int stu_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType){
     static Frame_info frame_cache[WINDOW_SIZE_BACK_N_FRAME];
-    static int front = 0;//窗口起始位置
-    static int length = 0;//窗口中已发出的帧个数
-    static queue<Frame_info> waiting_queue;
-    if(messageType == MSG_TYPE_SEND){
-        printf("MSG_TYPE_SEND\n");
+    static int front = 1;//窗口维护的起始seq_num
+    static int rear = 1;//窗口维护的结尾seq_num + 1
+    static queue<Frame_info> waiting_queue;//等待队列
+    if(messageType == MSG_TYPE_SEND){   
         //分配区域，缓存帧或者发出这个帧
         char *ptmp_frame=(char*)malloc(bufferSize);
-		memcpy(ptmp_frame, pBuffer);
+		memcpy(ptmp_frame, pBuffer, bufferSize);
         Frame_info fi;
         fi.pframe = (frame*)ptmp_frame;
         fi.size = bufferSize;
-        
-        if( length > WINDOW_SIZE_BACK_N_FRAME){//如果窗口大小过大
-            //帧放入等待队列
-            waiting_queue.push(fi);
-            return;
-
-        }else{//发送帧
-
-            frame_cache[(front + length) % WINDOW_SIZE_BACK_N_FRAME] = fi;
-            length++;
-            SendFRAMEPacket((unsigned char*)pBuffer, bufferSize);
-            return;
+        waiting_queue.push(fi);//帧放入等待队列
+        while( (rear - front) < WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){//如果窗口小于最大窗口
+            Frame_info qf = waiting_queue.front();//从等待队列中取出帧
+            waiting_queue.pop();
+            SendFRAMEPacket((unsigned char*)qf.pframe, qf.size);//发送帧
+            frame_cache[rear % WINDOW_SIZE_BACK_N_FRAME] = qf;
+            rear++;
         }
     }
-    else if(messageType == MSG_TYPE_TIMEOUT){
-        printf("MSG_TYPE_TIMEOUT\n");
-        //得到要发的帧的序列号
-        unsigned int seq_num = *((unsigned int*)pBuffer);
-        printf("the seq num is %d\n", seq_num);
-        SendFRAMEPacket((unsigned char*) frame_cache[(seq_num) % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[seq_num % WINDOW_SIZE_BACK_N_FRAME].size);
-
-        // if(seq_num == front){
-        //     for(int i = 0; i < size; ++i){
-        //         SendFRAMEPacket((unsigned char*) frame_cache[(front + i) % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[(front + i) % WINDOW_SIZE_BACK_N_FRAME].size);
-
-        //     }
-        // }
-        // else{
-        //     int rear = (front + length) % WINDOW_SIZE_BACK_N_FRAME;
-        //     for(int i = seq_num; i != rear; i = (i + 1) % WINDOW_SIZE_BACK_N_FRAME){//重新发送
-        //         SendFRAMEPacket((unsigned char*) frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].size);
-        //     }            
-        // }
-
-        return;
-    }
     else if(messageType == MSG_TYPE_RECEIVE){
-        printf("MSG_TYPE_RECEIVE\n");
         frame* received_frame = (frame*)pBuffer;
-        unsigned int ack_num = (unsigned int)ntohl(received_frame->head.ack);
-        printf("receive frame, the ack num is %d\n", ack_num);
+        unsigned int seq_num = (unsigned int)ntohl(received_frame->head.ack);
         frame_kind fkind = (frame_kind)(ntohl((unsigned long)received_frame->head.kind));
-        int rear = front + length;
         if(fkind == ack){//返回ack
-            if(ack_num >= front && ack_num < rear){
-                length -= ack_num - front + 1;//窗口缩小
-                front = ack_num + 1;
-                
+            if(seq_num >= front && seq_num < rear){
+                front = seq_num + 1;
                 //指导书写的发一个帧，其实应该尽量都发完
-                while(length <= WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){
+                while((rear - front) < WINDOW_SIZE_BACK_N_FRAME && !waiting_queue.empty()){
                     Frame_info fi = waiting_queue.front();
                     waiting_queue.pop();
-                    frame_cache[(front + length) % WINDOW_SIZE_BACK_N_FRAME] = fi;
+                    frame_cache[rear % WINDOW_SIZE_BACK_N_FRAME] = fi;
                     SendFRAMEPacket((unsigned char* )fi.pframe, fi.size);
-                    length++;
+                    rear++;
                 }
             }
 
-        }else if(fkind == nak){//nak,重发
-            printf("in received, fkind is nak\n");
-            SendFRAMEPacket((unsigned char*) frame_cache[ack_num % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[ack_num % WINDOW_SIZE_BACK_N_FRAME].size);
-            
         }
-
-    } 
+        else if(fkind == nak){//nak,重发
+            for(int i = front; i < rear; ++i){//找到要选择性重传的帧
+                unsigned int k =  (unsigned int)ntohl(frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe->head.seq);
+                if(k == seq_num){
+                    SendFRAMEPacket((unsigned char *)frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].pframe, frame_cache[i % WINDOW_SIZE_BACK_N_FRAME].size);
+                    break;
+                }
+                
+            }
+        }
+    }
+	return 0;
 }
